fba071d0f5697c41c4822633fe8f76fdb0056861
Kim <kimraniban@gmail.com>
2025-04-24 21:46:18 +0800

    Added Dog ID

--- core/models.py
+++ core/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from accounts.models import Barangay, PetOwnerProfile
 
 class Dog(models.Model):
+    dog_id = models.CharField(max_length=150, unique=True, blank=True, null=True)
+
     pet_profile = models.ImageField(upload_to='pet_profile/', blank=True,null=True, default='pet_profile/default_pet_profile.png')
     name = models.CharField(max_length=100)
     breed  = models.CharField(max_length=200, blank=True)
@@ -11,3 +13,14 @@ class Dog(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     barangay = models.ForeignKey(Barangay, on_delete= models.SET_NULL, blank=True, null=True)
     owner = models.ForeignKey(PetOwnerProfile, on_delete=models.SET_NULL, blank=True, null=True)
+
+    
+
+    def save(self, *args, **kwargs):
+            if not self.dog_id:
+                # Create a custom dog_id: combining dog's name, owner's name, and a count
+                owner_name = self.owner.first_name[:3] if self.owner else "OWN"  # Default to "OWN" if owner is not defined
+                dog_name = self.name[:3]  # Get the first 3 letters of the dog's name
+                count = Dog.objects.count() + 1  # Get the count of dogs, ensuring unique IDs
+                self.dog_id = f"{dog_name}-{owner_name}-{count:04d}"
+            super().save(*args, **kwargs)
