f06a4e64d874fe5e9fb757a727cbba63dafa439d
Kim <kimraniban@gmail.com>
2025-10-10 21:35:12 +0800

    Migrate to Postgresql

--- PawTraConnect/settings.py
+++ PawTraConnect/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'PawTraConnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pawtraconnect',
+        'USER': 'postgres',
+        'PASSWORD': 'kimas',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
